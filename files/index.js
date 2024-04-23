473afd5918b60a15cdbe9aece51523dcc8e275b6
Aleksandar Stanchev <altryx15@gmail.com>
2024-04-23 18:17:01 +0300

    Still working on it

---
+++ files/index.js
@@ -0,0 +1,33 @@
+const grid = document.querySelector('.grid') //I'm looking for the grid class (kazvash mu da tursi tova)
+const blockWidth = 100
+const blockHeight = 20
+
+//create Block
+class Block {
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis, yAxis]
+        this.bottomRight = [xAxis + blockWidth, yAxis]
+        this.topLeft = [xAxis, yAxis + blockHeight]
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+    }
+}
+
+// draw all my blocks
+const blocks = [
+    new Block(10, 270)
+]
+
+
+//draw my block
+function addBlocks() {
+ 
+   for (let i =0; i <blocks.length;i++) {
+    const block = document.createElement('div')
+    block.classList.add('block')
+    block.style.left = blocks[i].bottomleft[0]
+    block.style.bottom = '50px'
+    grid.appendChild(block)
+   }
+}
+
+addBlocks()
