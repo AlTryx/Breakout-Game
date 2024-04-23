71c94ba396fd5f37e92199da51a37f9ca569264c
Aleksandar Stanchev <altryx15@gmail.com>
2024-04-23 18:27:02 +0300

    Creating new blocks to the game
    I've created 15 blocks and set the first one's xAxis and yAxis. More to come soon

--- files/index.js
+++ files/index.js
@@ -5,15 +5,29 @@ const blockHeight = 20
 //create Block
 class Block {
     constructor(xAxis, yAxis) {
-        this.bottomLeft = [xAxis, yAxis]
-        this.bottomRight = [xAxis + blockWidth, yAxis]
-        this.topLeft = [xAxis, yAxis + blockHeight]
-        this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+        this.bottomLeft = [xAxis, yAxis]// 10 , 270
+        this.bottomRight = [(xAxis + blockWidth), yAxis] // 110, 270
+        this.topLeft = [xAxis, (yAxis + blockHeight)] // 10, 290
+        this.topRight = [(xAxis + blockWidth), (yAxis + blockHeight)] //110, 290
     }
 }
 
 // draw all my blocks
 const blocks = [
+    new Block(10, 270)
+    new Block(10, 270)
+    new Block(10, 270)
+    new Block(10, 270)
+    new Block(10, 270)
+    new Block(10, 270)
+    new Block(10, 270)
+    new Block(10, 270)
+    new Block(10, 270)
+    new Block(10, 270)
+    new Block(10, 270)
+    new Block(10, 270)
+    new Block(10, 270)
+    new Block(10, 270)
     new Block(10, 270)
 ]
 
@@ -24,8 +38,10 @@ function addBlocks() {
    for (let i =0; i <blocks.length;i++) {
     const block = document.createElement('div')
     block.classList.add('block')
-    block.style.left = blocks[i].bottomleft[0]
-    block.style.bottom = '50px'
+
+    block.style.left = blocks[i].bottomLeft[0] + 'px'
+    block.style.bottom = blocks[i].bottomLeft[1] + 'px'
+
     grid.appendChild(block)
    }
 }
